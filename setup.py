dfb41aeb5c9a37e40a0330d7677d2dbc26913c35
P. J. Reed <phillipreed@hatchbed.com>
2021-11-20 21:27:26 +0000

    ROS Noetic
    This makes several updates for ROS Noetic compatibility, including:
    - Fixing Python exception syntax
    - Cleaning up rosdeps
    - Switching to setuptools
    
    Signed-off-by: P. J. Reed <phillipreed@hatchbed.com>

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['camera_info_manager'],
-    package_dir={'': 'src'},
-    install_requires=['rospkg', 'yaml'],
+    package_dir={'': 'src'}
     )
 
 setup(**d)
